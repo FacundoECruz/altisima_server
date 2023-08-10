5caaedb62ee6a645405f7395f817bb97e9fda989
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-08-10 08:11:48 -0300

    moved validation logic to middleware

--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,32 @@ import Player from "../models/Player.js";
 import mongoose from "mongoose";
 const router = express.Router();
 
+async function validateUserData(req, res, next) {
+  const { username, email } = req.body;
+
+  const isUsernameRepeated = await User.findOne({ username: username });
+  const isEmailRepeated = await User.findOne({ email: email });
+  const isPlayerNameRepeated = await Player.findOne({ username: username });
+
+  if (
+    isUsernameRepeated !== null ||
+    isEmailRepeated !== null ||
+    isPlayerNameRepeated !== null
+  ) {
+    return res.status(400).json({
+      error: "El nombre de usuario o correo electrónico ya están registrados",
+    });
+  }
+
+  next();
+}
+
 router.get("/", async (req, res) => {
   try {
     const users = await User.find();
     res.json(users);
   } catch (error) {
-    res.status(500).json({ message: "Error retrieving users", error });
+    res.status(500).json({ message: "No se pudo recuperar los usuarios", error });
   }
 });
 
@@ -19,28 +39,20 @@ router.get("/:username", async (req, res) => {
     const user = await User.find({ username: username }); //
     res.json(user);
   } catch (error) {
-    res.status(500).json({ message: "Error retrieving user", error });
+    res.status(500).json({ message: "No se pudo recuperar el usuario", error });
   }
 });
 
-router.post("/", async (req, res) => {
-  const { username, email } = req.body;
-  
-  const isUsernameRepeated = await User.exists({ username });
-  const isEmailRepeated = await User.exists({ email });
-  const isPlayerNameRepeated = await Player.exists({ username })
-  if (isUsernameRepeated || isEmailRepeated || isPlayerNameRepeated) {
-    return res.status(400).json("El nombre de usuario o correo electrónico ya están registrados");
-  }
+router.post("/", validateUserData, async (req, res) => {
   const user = new User(req.body);
   const player = new Player(req.body);
   try {
     await user.save();
-    await player.save()
+    await player.save();
     console.log(user);
     return res.status(201).json(user);
   } catch (err) {
-    console.log(err)
+    console.log(err);
     return res.status(400).json(err.message);
   }
 });
@@ -50,16 +62,16 @@ router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ username });
     if (!user) {
-      return res.status(401).json({ message: "Invalid username or password" });
+      return res.status(401).json({ message: "Usuario o contraseña invalidos" });
     }
     const passwordMatch = user.password === password;
     if (!passwordMatch) {
-      return res.status(401).json({ message: "Invalid username or password" });
+      return res.status(401).json({ message: "Usuario o contraseña invalidos" });
     }
     res.status(200).json(user);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Server error" });
+    res.status(500).json({ message: "Error del servidor" });
   }
 });
 
@@ -75,10 +87,10 @@ router.patch("/:username", async (req, res) => {
     new: true,
   });
   if (!updatedUser) {
-    return res.status(404).json({ message: "User not found" });
+    return res.status(404).json({ message: "Usuario no encontrado" });
   }
   if (!updatedPlayer) {
-    return res.status(404).json({ message: "User not found" });
+    return res.status(404).json({ message: "Usuario no encontrado" });
   }
 
   res.status(200).json(updatedUser);
@@ -88,14 +100,14 @@ router.delete("/:id", async (req, res) => {
   const userId = req.params.id;
 
   if (!mongoose.Types.ObjectId.isValid(userId)) {
-    return res.status(400).json({ message: "Invalid user ID" });
+    return res.status(400).json({ message: "User ID invalida" });
   }
   const deletedUser = await User.findByIdAndDelete(userId);
   if (!deletedUser) {
-    return res.status(404).json({ message: "User not found" });
+    return res.status(404).json({ message: "Usuario no encontrado" });
   }
 
-  res.status(200).json({ message: "Successful deleted" });
+  res.status(200).json({ message: "Borrado exitosamente" });
 });
 
 export { router };
